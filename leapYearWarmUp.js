5a947702d62750758dd2b9f508ecfcbef78515dc
malsaadan <mmalsaadan@gmail.com>
2020-01-23 11:02:31 +0300

    warmup solution

--- leapYearWarmUp.js
+++ leapYearWarmUp.js
@@ -1,6 +1,6 @@
 const isLeapYear = function (){
     let year = prompt('Please enter the year to be checked: ');
-    if(year % 4 === 0 || year % 100 === 0 || year % 400 === 0){
+    if((year % 100 !== 0 || year % 400 == 0) && year % 4 === 0){
         return true;
     }
     else {
